881968b3706b3fee7ff077edc23e378e21eaf940
soilee <hande.sof@gmail.com>
2020-07-21 17:57:33 +0900

    ex05

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,24 +6,11 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 20:46:54 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/19 22:42:28 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/21 17:57:03 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	src_len(char *str, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (*(str + i) != '\0')
-		i++;
-	if (i > size)
-		return size;
-	else
-		return i;
-}
-
-unsigned int	dest_len(char *str)
+unsigned int	str_len(char *str)
 {
 	int i;
 
@@ -35,24 +22,24 @@ unsigned int	dest_len(char *str)
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
 	unsigned int	str_i;
-	
-	i = 0;
-	str_i = dest_len(dest) + src_len(src, size);
-	size -= 1;
-	while (i <= size)
+	unsigned int	dest_i;
+
+	if (str_len(dest) > size)
+		str_i = str_len(src) + size;
+	else if (str_len(dest) <= size)
+		str_i = str_len(dest) + str_len(src);
+	dest_i = str_len(dest);
+	while (dest_i < size)
 	{
-		if (dest_len(dest) > size)
-			break;
-		else if (*(dest + i) == '\0' && i < size)
+		if (*(dest + dest_i) == '\0' && dest_i < size - 1)
 		{
-			*(dest + i) = *src;
+			*(dest + dest_i) = *src;
 			src++;
 		}
-		else if (i == size)
-			*(dest + i) = '\0';
-		i++;
+		else if (dest_i == size)
+			*(dest + dest_i) = '\0';
+		dest_i++;
 	}
 	return (str_i);
 }
--- ex05/main.c
+++ ex05/main.c
@@ -6,21 +6,21 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 21:16:36 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/19 22:30:47 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/21 17:52:07 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-unsigned int	tft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 
 int				main(void)
 {
 	char dest1[50] = "abc";
 	char dest2[50] = "abc";
 	char src[] = "012309383";
-	unsigned int size = 1;
-	printf("%s, %u\n%s, %lu\n", dest1, tft_strlcat(dest1, src, size), dest2, strlcat(dest2, src, size));
+	unsigned int size = 0;
+	printf("%s, %u\n%s, %lu\n", dest1, ft_strlcat(dest1, src, size), dest2, strlcat(dest2, src, size));
 	return (0);
 }
